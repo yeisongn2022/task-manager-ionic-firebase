c6cb2f43b1b2c77c4b7d7f97bab91c366c32dcac
yeisongn2022 <116914144+yeisongn2022@users.noreply.github.com>
2025-12-30 16:39:32 -0500

    Versión final: Funcionalidad completa y despliegue Android exitoso

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBu_vcgWFrOuRSwxPkyNFAY3zZ4Ngeb1_M",
+    authDomain: "mistareasapp-59b8e.firebaseapp.com",
+    projectId: "mistareasapp-59b8e",
+    storageBucket: "mistareasapp-59b8e.firebasestorage.app",
+    messagingSenderId: "1064148097974",
+    appId: "1:1064148097974:web:fd87cf6748a4ffee71bf17"
+  }
 };
